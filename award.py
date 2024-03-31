54abf15e4f1c9202542f7ba746b6743249691f8a
[Elhadji] <[elhadjidiallo2@gmail.com]>
2024-03-31 23:49:59 +0100

    Add input for shot put time

--- award.py
+++ award.py
@@ -3,20 +3,20 @@ PROVINCIAL_COLOURS_MAX_TIME = 100
 PROVISIONAL_HALF_COLOURS_MAX_TIME = 105
 PROVINCIAL_SCROLL_MAX_TIME = 110
 
-swimming_time = int(input("Enter swimming time. "))
-cycling_time = int(input("Enter cycling time. "))
-running_time = int(input("Enter running time. "))
+swimming_time = int(input("Enter swimming time: "))
+cycling_time = int(input("Enter cycling time: "))
+running_time = int(input("Enter running time: "))
+shotput_time = int(input("Enter shot put time: "))  # New shot put time input
 
-total_time = swimming_time + cycling_time + running_time
+# Assuming the shot put time should be added to the total time
+total_time = swimming_time + cycling_time + running_time + shotput_time
 
-if total_time <= 100:
+if total_time <= PROVINCIAL_COLOURS_MAX_TIME:
     print("Provincial Colours")
-
-elif (total_time >= 101) and (total_time <= 105):
+elif total_time <= PROVISIONAL_HALF_COLOURS_MAX_TIME:
     print("Provisional Half Colours")
-
-elif (total_time >= 106) and (total_time <= 110):
+elif total_time <= PROVINCIAL_SCROLL_MAX_TIME:
     print("Provincial Scroll")
-
 else:
     print("No award")
+
